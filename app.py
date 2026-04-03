b2dc088493660ccb935c7bfa41dbd43ece757614
marcmeju <marcelmejulu@gmail.com>
2026-04-02 20:04:01 -0700

    set up db, migrations

--- app.py
+++ app.py
@@ -1,19 +1,34 @@
 from flask import Flask
-import models
+import os
+from dotenv import load_dotenv
+from models import db, init_app, User
+from routes import user_blueprint
+from flask_migrate import Migrate
+
+# Load the environment variables from the .env file
+load_dotenv()
 
 #initialize the application
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
+
+# Ensure instance folder exists
+try:
+    os.makedirs(app.instance_path, exist_ok=True)
+except OSError:
+    pass
 
 #database configuration
-app.config['SECRET_KEY'] = '' 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SECRET_KEY'] = os.getenv('USER_DB_SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(app.instance_path, 'user.db')}"
 app.config['SQL_ALCHEMY_TRACK_MODIFICATIONS'] = False
 
-#initialize the database
-models.init_app(app)
+#initialize the database and migration
+init_app(app)
+migrate = Migrate(app, db)
 
 #import the routes
-app.register_blueprint(routes.user_blueprint)
+app.register_blueprint(user_blueprint)
 
 #run the application
-app.run()
+if __name__ == '__main__':
+    app.run(debug=True, port=5003)
--- models.py
+++ models.py
@@ -9,16 +9,16 @@ def init_app(app):
 
 #define the User model
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True),
-    username = db.Column(db.String(80), unique=True, nullable=False),
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False),    
-    is_active = db.Column(db.Boolean, default=True), 
-    is_admin = db.Column(db.Boolean, default=False),
-    is_authenticated = db.Column(db.Boolean, default=False),
+    password = db.Column(db.String(120), nullable=False)    
+    is_active = db.Column(db.Boolean, default=True) 
+    is_admin = db.Column(db.Boolean, default=False)
+    is_authenticated = db.Column(db.Boolean, default=False)
 
 
-    def __repr__(self)__:
+    def __repr__(self):
         return f'<User {self.id}, {self.username}>'  
     
     def serialize(self):
